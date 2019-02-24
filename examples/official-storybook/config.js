86bd376e003b75e387e4a4d266b8b4fe3a4658b0
Gaëtan Maisse <gmaisse@leadformance.com>
2019-02-24 20:58:39 +0100

    fix: fix `config` a11y option attribute in examples/official-storybook

--- examples/official-storybook/config.js
+++ examples/official-storybook/config.js
@@ -42,7 +42,7 @@ addDecorator(storyFn => (
 
 addParameters({
   a11y: {
-    configure: {},
+    config: {},
     options: {
       checks: { 'color-contrast': { options: { noScroll: true } } },
       restoreScroll: true,
